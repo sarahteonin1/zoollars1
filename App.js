01bdbe4cc63ca0e7aa0a80be7c7a877cda3d5c92
sarahteonin <E1249329@u.nus.edu>
2024-05-28 00:04:07 +0800

    added 'add expenditure' page in homescreen

--- App.js
+++ App.js
@@ -1,56 +1,96 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { TouchableOpacity } from 'react-native';
+import { Entypo, Ionicons, Feather } from '@expo/vector-icons';
+
 import HomeScreen from './HomeScreen';
 import ProfileScreen from './ProfileScreen';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createDrawerNavigator } from '@react-navigation/drawer';
 import SpendingScreen from './SpendingScreen';
 import BudgetScreen from './BudgetScreen';
 import GoalsScreen from './GoalsScreen';
 import ChallengesScreen from './ChallengesScreen';
 import FriendsScreen from './FriendsScreen';
 import StoreScreen from './StoreScreen';
+import AddExpenditureScreen from './AddExpenditure';
 
+const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
 const StackNav = () => {
-  const Stack = createNativeStackNavigator();
+  const navigation = useNavigation();
   return (
     <Stack.Navigator
       screenOptions={{
-        statusBarColor: '6E9277',
-        headerStyle: {
-          backgroundColor: '6E9277'
-        },
-        headerTintColor: '6E9277',
         headerTitleAlign: 'center',
-      }}>
-      <Stack.Screen name="Profile" component={ProfileScreen} />
-      <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="Spendings Overview" component={SpendingScreen} />
-      <Stack.Screen name="Budget Overview" component={BudgetScreen} />
-      <Stack.Screen name="Goals" component={GoalsScreen} />
-      <Stack.Screen name="Challenges" component={ChallengesScreen} />
-      <Stack.Screen name="Store" component={StoreScreen} />
-      <Stack.Screen name="Friends" component={FriendsScreen} />
+        headerLeft: () => (
+          <TouchableOpacity onPress={() => navigation.toggleDrawer()}>
+            <Feather name="menu" size={24} color="black" paddingLeft={10} />
+          </TouchableOpacity>
+        ),
+        headerRight: () => (
+            <TouchableOpacity
+              onPress={() => navigation.navigate('Add Expenditure')}
+            >
+              <Entypo 
+              name="circle-with-plus" 
+              size={28} 
+              color="#6E9277"
+              paddingRight={10} 
+              />
+            </TouchableOpacity>
+          ),
+        }}
+      >
+        <Stack.Screen 
+        name="Welcome," 
+        component={HomeScreen}
+        />
+        <Stack.Screen name="Add Expenditure" component={AddExpenditureScreen} 
+          options={{
+            headerLeft: () => {
+              const navigation = useNavigation();
+              return(
+                <TouchableOpacity
+                  onPress={() => navigation.goBack()}
+                >
+                  <Ionicons name="chevron-back-outline" size={24} color="black"/>
+                </TouchableOpacity>
+              );
+            },
+            headerRight: () => null,
+            headerTitle: '', 
+            presentation: 'modal',
+          }}
+        />
     </Stack.Navigator>
   );
 }
 
-export default function App() {
-  const Drawer = createDrawerNavigator();
+
+const DrawerNav = () => {
   return (
-    <NavigationContainer>
-      <Drawer.Navigator>
-        <Drawer.Screen name="My Profile" component={ProfileScreen} />
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="Spendings Overview" component={SpendingScreen} />
-        <Drawer.Screen name="Budget Overview" component={BudgetScreen} />
-        <Drawer.Screen name="Goals" component={GoalsScreen} />
-        <Drawer.Screen name="Challenges" component={ChallengesScreen} />
-        <Drawer.Screen name="Store" component={StoreScreen} />
-        <Drawer.Screen name="Friends" component={FriendsScreen} />
-      </Drawer.Navigator>
-    </NavigationContainer>
+    <Drawer.Navigator initialRouteName="Home">
+      <Drawer.Screen name="My Profile" component={ProfileScreen} />
+      <Drawer.Screen name="Home" 
+      component={StackNav} 
+      options={{ headerShown: false}}
+      />
+      <Drawer.Screen name="Spendings Overview" component={SpendingScreen} />
+      <Drawer.Screen name="Budget Overview" component={BudgetScreen} />
+      <Drawer.Screen name="Goals" component={GoalsScreen} />
+      <Drawer.Screen name="Challenges" component={ChallengesScreen} />
+      <Drawer.Screen name="Store" component={StoreScreen} />
+      <Drawer.Screen name="Friends" component={FriendsScreen} />
+    </Drawer.Navigator>
+  );
+}
+export default function App() {
+  return (
+  <NavigationContainer>
+    <DrawerNav />
+  </NavigationContainer>
   );
 }
