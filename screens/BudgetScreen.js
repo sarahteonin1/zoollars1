590277de1f47939ce02ad6938568b3ec2c03cdf2
suddernpy <rrakshana24@gmail.com>
2024-06-27 18:19:20 +0800

    created graphs for weekly and monthly

--- screens/BudgetScreen.js
+++ screens/BudgetScreen.js
@@ -1,4 +1,4 @@
-import React , { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { doc, collection, query, where, onSnapshot } from 'firebase/firestore';
 import { db } from '../firebaseConfig';
 import { View, Text, StyleSheet, ScrollView, TouchableOpacity, Dimensions } from 'react-native';
@@ -9,53 +9,231 @@ const windowWidth = Dimensions.get('window').width;
 
 export default function BudgetScreen({ userData }) {
   const [activePill, setActivePill] = useState('daily');
-  const [todayTotalSpendings, setTodayTotalSpendings] = useState(0);
+  const [totalSpendings, setTotalSpendings] = useState(0);
   const [chartData, setChartData] = useState([]);
+  const [weeklySpendingData, setWeeklySpendingData] = useState([]);
+  const [monthlySpendingData, setMonthlySpendingData] = useState([]);
+  const [savingsData, setSavingsData] = useState([]);
 
   useEffect(() => {
-    const fetchTodayTotalSpendings = async () => {
-      const userDocRef = doc(db, 'users', userData.email);
-      const today = new Date();
-      const todayDateString = today.toDateString();
+    if (activePill === 'daily') {
+      fetchTodayTotalSpendings();
+    } else if (activePill === 'weekly') {
+      fetchWeeklyTotalSpendings();
+      fetchWeeklySpendingByDay();
+    } else if (activePill === 'monthly') {
+      fetchMonthlyTotalSpendings();
+      fetchMonthlySpendingByMonth();
+    }
+  }, [activePill]);
 
-      const expensesRef = collection(db, `users/${userDocRef.id}/expenses`);
-      const q = query(expensesRef, where('date', '==', todayDateString));
+  const fetchTodayTotalSpendings = async () => {
+    const userDocRef = doc(db, 'users', userData.email);
+    const today = new Date();
+    const todayDateString = today.toDateString();
 
-      const unsubscribe = onSnapshot(q, (querySnapshot) => {
-        const expenses = [];
-        querySnapshot.forEach((doc) => {
-          expenses.push(doc.data());
-        });
+    const expensesRef = collection(db, `users/${userDocRef.id}/expenses`);
+    const q = query(expensesRef, where('date', '==', todayDateString));
 
-        let total = 0;
-        let categorySpendings = {};
-
-        expenses.forEach((expense) => {
-          if (expense.type === 'Expenditure') {
-            total += parseFloat(expense.amount);
-            if (categorySpendings[expense.category]) {
-              categorySpendings[expense.category] += parseFloat(expense.amount);
-            } else {
-              categorySpendings[expense.category] = parseFloat(expense.amount);
-            }
-          }
-        });
-
-        setTodayTotalSpendings(total);
-
-        const formattedChartData = Object.keys(categorySpendings).map(category => ({
-          value: categorySpendings[category],
-          label: category,
-        }));
-
-        setChartData(formattedChartData);
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const expenses = [];
+      querySnapshot.forEach((doc) => {
+        expenses.push(doc.data());
       });
 
-      return () => unsubscribe();
-    };
+      let total = 0;
+      let categorySpendings = {};
 
-    fetchTodayTotalSpendings();
-  }, []);
+      expenses.forEach((expense) => {
+        if (expense.type === 'Expenditure') {
+          total += parseFloat(expense.amount);
+          if (categorySpendings[expense.category]) {
+            categorySpendings[expense.category] += parseFloat(expense.amount);
+          } else {
+            categorySpendings[expense.category] = parseFloat(expense.amount);
+          }
+        }
+      });
+
+      setTotalSpendings(total);
+
+      const formattedChartData = Object.keys(categorySpendings).map(category => ({
+        value: categorySpendings[category],
+        label: category,
+      }));
+
+      setChartData(formattedChartData);
+    });
+
+    return () => unsubscribe();
+  };
+
+  const fetchWeeklyTotalSpendings = async () => {
+    const userDocRef = doc(db, 'users', userData.email);
+    const today = new Date();
+    const startOfWeek = new Date(today.setDate(today.getDate() - today.getDay()));
+    const endOfWeek = new Date(startOfWeek);
+    endOfWeek.setDate(endOfWeek.getDate() + 6);
+
+    const expensesRef = collection(db, `users/${userDocRef.id}/expenses`);
+    const q = query(expensesRef, where('date', '>=', startOfWeek.toDateString()), where('date', '<=', endOfWeek.toDateString()));
+
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const expenses = [];
+      querySnapshot.forEach((doc) => {
+        const data = doc.data();
+        data.date = new Date(data.date); // Convert string to Date
+        expenses.push(data);
+      });
+
+      let total = 0;
+      let categorySpendings = {};
+
+      expenses.forEach((expense) => {
+        if (expense.type === 'Expenditure') {
+          total += parseFloat(expense.amount);
+          if (categorySpendings[expense.category]) {
+            categorySpendings[expense.category] += parseFloat(expense.amount);
+          } else {
+            categorySpendings[expense.category] = parseFloat(expense.amount);
+          }
+        }
+      });
+
+      setTotalSpendings(total);
+
+      const formattedChartData = Object.keys(categorySpendings).map(category => ({
+        value: categorySpendings[category],
+        label: category,
+      }));
+
+      setChartData(formattedChartData);
+    });
+
+    return () => unsubscribe();
+  };
+
+  const fetchWeeklySpendingByDay = async () => {
+    const userDocRef = doc(db, 'users', userData.email);
+    const today = new Date();
+    const startOfWeek = new Date(today.setDate(today.getDate() - today.getDay()));
+    const endOfWeek = new Date(startOfWeek);
+    endOfWeek.setDate(endOfWeek.getDate() + 6);
+
+    const expensesRef = collection(db, `users/${userDocRef.id}/expenses`);
+    const q = query(expensesRef, where('date', '>=', startOfWeek.toDateString()), where('date', '<=', endOfWeek.toDateString()));
+
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const expenses = [];
+      querySnapshot.forEach((doc) => {
+        const data = doc.data();
+        data.date = new Date(data.date); // Convert string to Date
+        expenses.push(data);
+      });
+
+      let daySpendings = {};
+
+      expenses.forEach((expense) => {
+        if (expense.type === 'Expenditure') {
+          const day = expense.date.getDay();
+          if (daySpendings[day]) {
+            daySpendings[day] += parseFloat(expense.amount);
+          } else {
+            daySpendings[day] = parseFloat(expense.amount);
+          }
+        }
+      });
+
+      const formattedChartData = Object.keys(daySpendings).map(day => ({
+        value: daySpendings[day],
+        label: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'][day],
+      }));
+
+      setWeeklySpendingData(formattedChartData);
+    });
+
+    return () => unsubscribe();
+  };
+
+  const fetchMonthlyTotalSpendings = async () => {
+    const userDocRef = doc(db, 'users', userData.email);
+    const today = new Date();
+    const startOfMonth = new Date(today.getFullYear(), today.getMonth(), 1);
+    const endOfMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0);
+
+    const expensesRef = collection(db, `users/${userDocRef.id}/expenses`);
+    const q = query(expensesRef, where('date', '>=', startOfMonth.toDateString()), where('date', '<=', endOfMonth.toDateString()));
+
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const expenses = [];
+      querySnapshot.forEach((doc) => {
+        expenses.push(doc.data());
+      });
+
+      let total = 0;
+      let categorySpendings = {};
+
+      expenses.forEach((expense) => {
+        if (expense.type === 'Expenditure') {
+          total += parseFloat(expense.amount);
+          if (categorySpendings[expense.category]) {
+            categorySpendings[expense.category] += parseFloat(expense.amount);
+          } else {
+            categorySpendings[expense.category] = parseFloat(expense.amount);
+          }
+        }
+      });
+
+      setTotalSpendings(total);
+
+      const formattedChartData = Object.keys(categorySpendings).map(category => ({
+        value: categorySpendings[category],
+        label: category,
+      }));
+
+      setChartData(formattedChartData);
+    });
+
+    return () => unsubscribe();
+  };
+
+  const fetchMonthlySpendingByMonth = async () => {
+    const userDocRef = doc(db, 'users', userData.email);
+
+    const expensesRef = collection(db, `users/${userDocRef.id}/expenses`);
+    const q = query(expensesRef);
+
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const expenses = [];
+      querySnapshot.forEach((doc) => {
+        const data = doc.data();
+        data.date = new Date(data.date); // Convert string to Date
+        expenses.push(data);
+      });
+
+      let monthSpendings = {};
+
+      expenses.forEach((expense) => {
+        if (expense.type === 'Expenditure') {
+          const month = expense.date.getMonth();
+          if (monthSpendings[month]) {
+            monthSpendings[month] += parseFloat(expense.amount);
+          } else {
+            monthSpendings[month] = parseFloat(expense.amount);
+          }
+        }
+      });
+
+      const formattedChartData = Object.keys(monthSpendings).map(month => ({
+        value: monthSpendings[month],
+        label: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'][month],
+      }));
+
+      setMonthlySpendingData(formattedChartData);
+    });
+
+    return () => unsubscribe();
+  };
 
   const handleWeeklyPress = () => {
     setActivePill('weekly');
@@ -97,14 +275,15 @@ export default function BudgetScreen({ userData }) {
         </View>
       </View>
 
-      <ScrollView contentContainerStyle={styles.chartContainer}>
-      {/* Zoo Section */}
-      <Zoo />
+      <ScrollView contentContainerStyle={styles.scrollContent}>
+        {/* Zoo Section */}
+        <Zoo />
 
-      {/* Scrollable Chart Section */}
-      
+        {/* Scrollable Chart Section */}
         <View style={styles.card}>
-          <Text style={styles.chartTitle}>Today's Spendings</Text>
+          <Text style={styles.chartTitle}>
+            {activePill === 'daily' ? "Today's Spendings" : activePill === 'weekly' ? "This Week's Spendings" : "This Month's Spendings"}
+          </Text>
           <BarChart 
             key={JSON.stringify(chartData)}
             data={chartData}
@@ -124,14 +303,85 @@ export default function BudgetScreen({ userData }) {
             )}
           />
         </View>
+
+        {activePill === 'weekly' && (
+          <View style={styles.card}>
+            <Text style={styles.chartTitle}>Spending by Day</Text>
+            <BarChart 
+              key={JSON.stringify(weeklySpendingData)}
+              data={weeklySpendingData}
+              frontColor={'#6E9277'}
+              barWidth={50}
+              spacing={70}
+              barBorderTopLeftRadius={4}
+              barBorderTopRightRadius={4}
+              xAxisLength={windowWidth - 110}
+              rulesLength={windowWidth - 110}
+              xAxisLabelTextStyle={styles.label}
+              showFractionalValues={false}
+              initialSpacing={10}
+              noOfSections={4} // Adjust as needed
+              renderTooltip={({ value }) => (
+                <Text style={{ color: '#000', fontWeight: 'bold', paddingBottom: 2 }}>{value.toFixed(2)}</Text>
+              )}
+            />
+          </View>
+        )}
+
+        {activePill === 'monthly' && (
+          <>
+            <View style={styles.card}>
+              <Text style={styles.chartTitle}>Spending by Month</Text>
+              <BarChart 
+                key={JSON.stringify(monthlySpendingData)}
+                data={monthlySpendingData}
+                frontColor={'#6E9277'}
+                barWidth={50}
+                spacing={70}
+                barBorderTopLeftRadius={4}
+                barBorderTopRightRadius={4}
+                xAxisLength={windowWidth - 110}
+                rulesLength={windowWidth - 110}
+                xAxisLabelTextStyle={styles.label}
+                showFractionalValues={false}
+                initialSpacing={10}
+                noOfSections={4} // Adjust as needed
+                renderTooltip={({ value }) => (
+                  <Text style={{ color: '#000', fontWeight: 'bold', paddingBottom: 2 }}>{value.toFixed(2)}</Text>
+                )}
+              />
+            </View>
+            <View style={styles.card}>
+              <Text style={styles.chartTitle}>Savings by Month</Text>
+              <BarChart 
+                key={JSON.stringify(savingsData)}
+                data={savingsData}
+                frontColor={'#6E9277'}
+                barWidth={50}
+                spacing={70}
+                barBorderTopLeftRadius={4}
+                barBorderTopRightRadius={4}
+                xAxisLength={windowWidth - 110}
+                rulesLength={windowWidth - 110}
+                xAxisLabelTextStyle={styles.label}
+                showFractionalValues={false}
+                initialSpacing={10}
+                noOfSections={4} // Adjust as needed
+                renderTooltip={({ value }) => (
+                  <Text style={{ color: '#000', fontWeight: 'bold', paddingBottom: 2 }}>{value.toFixed(2)}</Text>
+                )}
+              />
+            </View>
+          </>
+        )}
       </ScrollView>
     </View>
   );
 }
-  
+
 const styles = StyleSheet.create({
   container: {
-    flexGrow: 1,
+    flex: 1,
     padding: 16,
     backgroundColor: '#fff',
   },
@@ -163,8 +413,8 @@ const styles = StyleSheet.create({
   pillContainer: {
     alignItems: 'center',
   },
-  chartContainer: {
-    flex: 1,
+  scrollContent: {
+    paddingBottom: 20,
   },
   card: {
     backgroundColor: '#fff',
@@ -190,5 +440,5 @@ const styles = StyleSheet.create({
     fontSize: 12,
     width: 60,
     alignSelf: 'center',
-}
+  }
 });
